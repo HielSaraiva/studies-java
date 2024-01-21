6a3adb8ac2966887f76df319256ba6363612c8c0
Hiel Saraiva Freitas de Queiroga <hielsaraiva11.hs@gmail.com>
2024-01-20 22:28:56 -0300

    studies about date and dateFormat

---
+++ extra/date/Data.java
@@ -0,0 +1,32 @@
+package udemy_java;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Data {
+   public static void main(String[] args) {
+
+      LocalDate d04 = LocalDate.parse("2022-07-20");
+      LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+      Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+
+      LocalDate pastWeekLocalDate = d04.minusWeeks(1);
+
+      System.out.println(d04 + " e " + pastWeekLocalDate);
+
+      DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+      System.out.println(fmt1.format(pastWeekLocalDate));
+
+      LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
+      LocalDateTime nextWeekLocalDateTime = d05.plusDays(7);
+
+      Duration t1 = Duration.between(pastWeekLocalDateTime, d05);
+
+      System.out.println("t1 dias = " + t1.toDays());
+
+   }
+}
---
+++ extra/date/TesteData.java
@@ -0,0 +1,25 @@
+package udemy_java;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class TesteData {
+   public static void main(String[] args) {
+      
+      DateTimeFormatter frmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+      Scanner input = new Scanner(System.in);
+
+      System.out.println("Digite uma data: (dd/MM/yyyy)");
+      String dateString = input.nextLine();
+
+      LocalDate date = LocalDate.parse(dateString, frmt1);
+
+      System.out.println("Data: " + frmt1.format(date));
+      System.out.println("Day of Month: " + date.getDayOfMonth());
+      System.out.println("Day of Year: " + date.getDayOfYear());
+      System.out.println("Month: " + date.getMonthValue());
+      System.out.println("Year: " + date.getYear());
+   }
+}
